e0b0b85e9d8d8ba7cbfa49510b00b09319222722
Anatoliy Anatoliy <86682525+ScepticismZbS@users.noreply.github.com>
2022-04-23 14:27:45 +0300

    Array Redux

--- deposit-project/src/App.js
+++ deposit-project/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import './App.css'
+import { addCustomerAction, removeCustomerAction } from "./store/customerReducer";
 
 function App() {
 
   const dispatch = useDispatch()
   const cash = useSelector(state => state.cash.cash)
+  const customers = useSelector(state => state.customers.customers)
 
   const addCash = (cash) => {
     dispatch({type: 'ADD_CASH', payload: cash})
@@ -15,14 +17,39 @@ function App() {
     dispatch({type: 'GET_CASH', payload: cash})
   }
 
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now()
+    }
+    dispatch(addCustomerAction(customer))
+  }
+
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id))
+  }
+
 
   return (
     <div className="App">
-      <div style={{fontSize: '3rem'}}>{cash}</div>
-      <div className="btns" style={{display: 'flex'}}>
+      <div className="balance">Баланс: {cash}</div>
+      <div className="btns">
         <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
         <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
+        <button onClick={() => addCustomer(prompt())}>Добавить клиента</button>
+        <button onClick={() => getCash(Number(prompt()))}>Удалить клиента</button>
       </div>
+      {customers.length > 0 ?
+        <div>
+          {customers.map( customer => 
+            <div onClick={() => removeCustomer(customer)} className="customers">{customer.name}</div>
+          )}
+        </div>
+        :
+        <div style={{fontSize: '2rem', marginTop: '20px'}}>
+          Клиенты отстутствуют!
+        </div>
+      }
       
     </div>
   );
